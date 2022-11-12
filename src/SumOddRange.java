643db76ce0dfb20ced99ac0e5d52df4470e55387
fdudaev <61507529+fdudaev@users.noreply.github.com>
2022-11-12 11:05:35 -0500

    Sum Odd Range project

---
+++ src/SumOddRange.java
@@ -0,0 +1,29 @@
+public class SumOddRange {
+
+    public static void main(String[] args) {
+        System.out.println(isOdd(45));
+
+    }
+    public static boolean isOdd(int number){
+        if (number <= 0) {
+            return false;
+        }
+        return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end){
+        if ( (end < start) || (start < 0) || (end < 0) ){
+            return -1;
+        }
+        int sum = 0;
+        for (int i = start; i<=end; i++){
+            if (isOdd(i)){
+                sum +=i;
+            }
+        }
+        return sum;
+
+    }
+
+}
+
